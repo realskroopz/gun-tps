f4a31b85df60843abb0a4582dfb52e6394c8def5
realskroopz <93640134+realskroopz@users.noreply.github.com>
2022-01-25 19:20:27 -0500

    Update guntps.lua

--- guntps.lua
+++ guntps.lua
@@ -1,3 +1,3 @@
 --skroopz on top little niggers
---If you see this contact me on discord  ឵)#9050, (copy and paste it because theres a invisible character)
+--If you see this contact me on discord  skroopz#9050
 loadstring(game:HttpGet("https://raw.githubusercontent.com/realskroopz/script/main/Survive%20and%20Kill%20the%20Killers%20in%20Area%2051%20Script.lua",true))()
